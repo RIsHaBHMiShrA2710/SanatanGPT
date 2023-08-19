834ed8069716fc8f0be0be6bbcec26b7daebeb7a
Rishabh <2710rismi@gmail.com>
2023-08-19 22:58:33 +0530

    expr

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
 import axios from 'axios';
 import { config } from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai'; // Import OpenAI API classes
+import { Configuration, OpenAIApi } from 'openai';
 
 config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(express.static('public'));
 
 // Configure OpenAI API
 const openAi = new OpenAIApi(
@@ -38,26 +34,17 @@ async function generateResponseFromOpenAI(inputText) {
   }
 }
 
-
-  app.post('/generate-response', async (req, res) => {
-    let inputText = req.body.inputText; // Use let instead of const
-    inputText = inputText + "The given query or statement should strictly be answered in the context of hinduism, vedic, sanatani and hindu philosophy. To convey your answer quote the hindu sacrad texts too. If you think that the query is unrelated to any hindu/sanatani thing then reply that there is not any relation you can find. Use pure honorific hindi for your reply.";
-    try {
-      const openaiResponse = await generateResponseFromOpenAI(inputText);
-      res.json({ response: openaiResponse });
-      console.log(openaiResponse);
-    } catch (error) {
-      console.error('Error generating response:', error.message);
-      res.status(500).json({ error: 'An error occurred while generating the response.' });
-    }
-  });
-  
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.post('/.netlify/functions/generate-response', async (req, res) => {
+  let inputText = req.body.inputText; // Use let instead of const
+  inputText = inputText + "The given query or statement should strictly be answered in the context of hinduism, vedic, sanatani and hindu philosophy. To convey your answer quote the hindu sacrad texts too. If you think that the query is unrelated to any hindu/sanatani thing then reply that there is not any relation you can find. Use pure honorific hindi for your reply.";
+  try {
+    const openaiResponse = await generateResponseFromOpenAI(inputText);
+    res.json({ response: openaiResponse });
+    console.log(openaiResponse);
+  } catch (error) {
+    console.error('Error generating response:', error.message);
+    res.status(500).json({ error: 'An error occurred while generating the response.' });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+export default app;
